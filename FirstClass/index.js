6a88f1e24b607325acce4bb12e304b0f83742107
arifweb2020 <arifhussainb2019@gmail.com>
2021-06-21 16:57:17 +0530

    file system

--- FirstClass/index.js
+++ FirstClass/index.js
@@ -1,7 +1,27 @@
 //console.log("welcome to node js");
 
-// creating file
+// creating new file and if file is present then it updates the file
 
 const myfile = require("fs")
 
-myfile.writeFileSync("read.txt" , "welcome to arif")
+//myfile.writeFileSync("read.txt" , "welcome to arif")
+
+
+// for append the data in exiting file
+
+//myfile.appendFileSync("read.txt" , " this nodejs tutorial")
+
+
+// to read the data
+
+const myfile1 = myfile.readFileSync("read.txt");
+
+console.log(myfile1);
+
+const org_data = myfile1.toString();
+
+console.log(org_data)
+
+// to rename file
+
+myfile.renameSync("read.txt" , "read-write.txt")
