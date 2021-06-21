4c762209078c4542dd9766da5f660ca16237684e
arifweb2020 <arifhussainb2019@gmail.com>
2021-06-21 17:55:57 +0530

    crud file system in node

---
+++ CRUD-FS_MODULE/index.js
@@ -0,0 +1,31 @@
+const mydata = require ("fs")
+
+// create folder
+
+//mydata.mkdirSync("Node-CRUD")
+
+// make one file in that folder
+
+//mydata.writeFileSync("Node-CRUD/bio.txt" , "About arif")
+
+// update the file with some text
+
+//mydata.appendFileSync("Node-CRUD/bio.txt" , "About arif , i m a react developer")
+
+// read the file without buffer
+
+//const data = mydata.readFileSync("Node-CRUD/bio.txt", "utf-8");
+//console.log(data)
+
+// rename file name
+
+//mydata.renameSync("Node-CRUD/bio.txt" , "Node-CRUD/myBio.txt")
+
+// to delete file
+
+//mydata.unlinkSync("Node-CRUD/myBio.txt")
+
+// to delete Folder
+
+//mydata.rmdirSync("Node-CRUD")
+
